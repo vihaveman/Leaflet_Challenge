7f8289736ab4ca617d1f2a12bdf6018c9dd15f03
Violetta Haveman <120049344+vihaveman@users.noreply.github.com>
2023-06-21 12:06:23 -0400

    Update logic.js

--- LeafletPart2/static/js/logic.js
+++ LeafletPart2/static/js/logic.js
@@ -1,107 +1,109 @@
-// API endpoint for earthquake data
-var earthquakeQueryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-var platesQueryUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+// Create the Leaflet map
+var map = L.map('map').setView([0, 0], 2);
 
+// Add tile layers
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  maxZoom: 18
+}).addTo(map);
 
-// Perform GET requests to the query URLs
-Promise.all([d3.json(earthquakeQueryUrl), d3.json(platesQueryUrl)])
-  .then(function (data) {
-    var earthquakeData = data[0];
-    var platesData = data[1];
+// Create separate layer groups for earthquakes and tectonic plates
+var earthquakesLayer = L.layerGroup().addTo(map);
+var tectonicPlatesLayer = L.layerGroup().addTo(map);
 
-    // Create a Leaflet map centered around the United States
-    var map = L.map("map").setView([37.09, -95.71], 4);
+// Create overlay control to toggle layers
+var overlayMaps = {
+  "Earthquakes": earthquakesLayer,
+  "Tectonic Plates": tectonicPlatesLayer
+};
 
-    // Create different base maps to choose from
-    var streetmap = L.tileLayer(
-      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      {
-        maxZoom: 19,
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      }
-    );
-    var darkmap = L.tileLayer(
-      "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
-      {
-        maxZoom: 19,
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      }
-    );
+L.control.layers(null, overlayMaps).addTo(map);
 
-    // Function to determine marker size based on magnitude
-    function getMarkerSize(magnitude) {
-      return magnitude * 4;
-    }
+// Function to get color based on earthquake depth
+function getMarkerColor(depth) {
+  // Define your own color ranges based on your dataset
+  if (depth < 30) {
+    return '#00ff00';
+  } else if (depth < 70) {
+    return '#ffff00';
+  } else {
+    return '#ff0000';
+  }
+}
 
-    // Function to determine marker color based on depth
-    function getMarkerColor(depth) {
-      if (depth < 10) {
-        return "#00FF00";
-      } else if (depth < 30) {
-        return "#FFFF00";
-      } else if (depth < 50) {
-        return "#FF9900";
-      } else if (depth < 70) {
-        return "#FF6600";
-      } else if (depth < 90) {
-        return "#FF3300";
-      } else {
-        return "#FF0000";
-      }
-    }
+// Create legend
+var legend = L.control({ position: "bottomright" });
 
-    // Create the overlay layers for earthquakes and tectonic plates
-    var earthquakeLayer = L.geoJSON(earthquakeData.features, {
+legend.onAdd = function (map) {
+  var div = L.DomUtil.create("div", "legend");
+
+  var grades = [0, 10, 30, 50, 70, 90];
+  var labels = [];
+  var legendInfo = "<h4>Depth (km)</h4>";
+
+  div.innerHTML = legendInfo;
+
+  for (var i = 0; i < grades.length; i++) {
+    div.innerHTML +=
+      '<i style="background:' +
+      getMarkerColor(grades[i] + 1) +
+      '"></i> ' +
+      grades[i] +
+      (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+  }
+
+  return div;
+};
+
+// Add legend to the map
+legend.addTo(map);
+
+// Retrieve earthquake GeoJSON data and create markers
+fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson')
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
       pointToLayer: function (feature, latlng) {
+        var magnitude = feature.properties.mag;
+        var depth = feature.geometry.coordinates[2];
+        var radius = Math.max(2, magnitude * 2);
+        var color = getMarkerColor(depth);
+
         return L.circleMarker(latlng, {
-          radius: getMarkerSize(feature.properties.mag),
-          fillColor: getMarkerColor(feature.geometry.coordinates[2]),
-          color: "#000",
+          radius: radius,
+          fillColor: color,
+          color: '#000',
           weight: 1,
           opacity: 1,
-          fillOpacity: 0.8,
+          fillOpacity: 0.8
         });
       },
       onEachFeature: function (feature, layer) {
-        layer.bindPopup(
-          "<h3>" +
-            feature.properties.place +
-            "</h3><hr><p>" +
-            new Date(feature.properties.time) +
-            "</p>" +
-            "<hr> <p> Earthquake Magnitude: " +
-            feature.properties.mag +
-            "</p>"
-        );
-      },
-    });
-
-    var platesLayer = L.geoJSON(platesData.features, {
-      style: {
-        color: "orange",
-        weight: 2,
-      },
-    });
-
-    // Create overlay groups for earthquakes and tectonic plates
-    var overlayMaps = {
-      Earthquakes: earthquakeLayer,
-      "Tectonic Plates": platesLayer,
-    };
-
-    // Add the base maps and overlay layers to the map
-    L.control.layers(
-      {
-        Streetmap: streetmap,
-        Darkmap: darkmap,
-      },
-      overlayMaps
-    ).addTo(map);
-
-    // Add earthquakes and streetmap layer to the map by default
-    streetmap.addTo(map);
-    earthquakeLayer.addTo(map);
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Depth: " + feature.geometry.coordinates[2] + " km");
+      }
+    }).addTo(earthquakesLayer);
   });
+
+// Retrieve tectonic plates GeoJSON data and add to the map
+fetch('data/PB2002_plates.json')
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
+      style: function (feature) {
+        return {
+          color: '#ff0000',
+          weight: 2
+        };
+      }
+    }).addTo(tectonicPlatesLayer);
+  });
+
+
+
+
+
+
+
+
+
 
