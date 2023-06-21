f3740f31d50540c1f6c6c199eb57ed45530261d9
Violetta Haveman <120049344+vihaveman@users.noreply.github.com>
2023-06-21 12:43:18 -0400

    Update logic.js

--- LeafletPart2/static/js/logic.js
+++ LeafletPart2/static/js/logic.js
@@ -1,102 +1,138 @@
-// Create the Leaflet map
-var map = L.map('map').setView([0, 0], 2);
+// API endpoint for earthquake data
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var tectonicPlatesUrl = 'data/PB2002_plates.json';
 
-// Add tile layers
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-  maxZoom: 18
-}).addTo(map);
-
-// Create separate layer groups for earthquakes and tectonic plates
-var earthquakesLayer = L.layerGroup().addTo(map);
-var tectonicPlatesLayer = L.layerGroup().addTo(map);
-
-// Create overlay control to toggle layers
-var overlayMaps = {
-  "Earthquakes": earthquakesLayer,
-  "Tectonic Plates": tectonicPlatesLayer
-};
-
-L.control.layers(null, overlayMaps).addTo(map);
-
-// Function to get color based on earthquake depth
-function getMarkerColor(depth) {
-  // Define your own color ranges based on your dataset
-  if (depth < 30) {
-    return '#00ff00';
-  } else if (depth < 70) {
-    return '#ffff00';
-  } else {
-    return '#ff0000';
-  }
-}
-
-// Create legend
-var legend = L.control({ position: "bottomright" });
-
-legend.onAdd = function (map) {
-  var div = L.DomUtil.create("div", "legend");
-
-  var grades = [0, 10, 30, 50, 70, 90];
-  var labels = [];
-  var legendInfo = "<h4>Depth (km)</h4>";
-
-  div.innerHTML = legendInfo;
-
-  for (var i = 0; i < grades.length; i++) {
-    div.innerHTML +=
-      '<i style="background:' +
-      getMarkerColor(grades[i] + 1) +
-      '"></i> ' +
-      grades[i] +
-      (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
-  }
-
-  return div;
-};
-
-// Add legend to the map
-legend.addTo(map);
-
-// Retrieve earthquake GeoJSON data and create markers
-fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson')
+// Perform a GET request to the query URL
+fetch(queryUrl)
   .then(response => response.json())
   .then(data => {
-    L.geoJSON(data, {
-      pointToLayer: function (feature, latlng) {
-        var magnitude = feature.properties.mag;
-        var depth = feature.geometry.coordinates[2];
-        var radius = Math.max(2, magnitude * 2);
-        var color = getMarkerColor(depth);
+    // Create a Leaflet map centered around the United States
+    var map = L.map("map").setView([37.09, -95.71], 4);
 
-        return L.circleMarker(latlng, {
-          radius: radius,
-          fillColor: color,
-          color: '#000',
-          weight: 1,
-          opacity: 1,
-          fillOpacity: 0.8
-        });
-      },
-      onEachFeature: function (feature, layer) {
-        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Depth: " + feature.geometry.coordinates[2] + " km");
+    // Create the tile layer for the base map
+    var streetmap = L.tileLayer(
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        maxZoom: 19,
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
       }
-    }).addTo(earthquakesLayer);
+    ).addTo(map);
+
+    // Create a layer group for tectonic plates
+    var tectonicPlatesLayer = L.layerGroup().addTo(map);
+
+    // Function to determine marker size based on magnitude
+    function getMarkerSize(magnitude) {
+      return magnitude * 4;
+    }
+
+    // Function to determine marker color based on depth
+    function getMarkerColor(depth) {
+      if (depth < 10) {
+        return "#00FF00";
+      } else if (depth < 30) {
+        return "#FFFF00";
+      } else if (depth < 50) {
+        return "#FF9900";
+      } else if (depth < 70) {
+        return "#FF6600";
+      } else if (depth < 90) {
+        return "#FF3300";
+      } else {
+        return "#FF0000";
+      }
+    }
+
+    // Function to create markers with tooltips
+    function createMarkers(feature, latlng) {
+      var options = {
+        radius: getMarkerSize(feature.properties.mag),
+        fillColor: getMarkerColor(feature.geometry.coordinates[2]),
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+      };
+
+      var marker = L.circleMarker(latlng, options);
+
+      // Create a tooltip with magnitude, location, and depth information
+      var tooltipContent =
+        "<strong>Magnitude:</strong> " +
+        feature.properties.mag +
+        "<br>" +
+        "<strong>Location:</strong> " +
+        feature.properties.place +
+        "<br>" +
+        "<strong>Depth:</strong> " +
+        feature.geometry.coordinates[2] +
+        " km";
+
+      // Bind the tooltip to the marker
+      marker.bindTooltip(tooltipContent, { sticky: true });
+
+      // Bind popup with additional information to each marker
+      marker.bindPopup(
+        "<h3>" +
+          feature.properties.place +
+          "</h3><hr><p>" +
+          new Date(feature.properties.time) +
+          "</p>" +
+          "<hr> <p> Earthquake Magnitude: " +
+          feature.properties.mag +
+          "</p>"
+      );
+
+      return marker;
+    }
+
+    // Create a GeoJSON layer containing the features array
+    var earthquakes = L.geoJSON(data.features, {
+      pointToLayer: createMarkers,
+    });
+
+    // Add earthquakes layer to the map
+    earthquakes.addTo(map);
+
+    // Retrieve tectonic plates GeoJSON data and add to the map
+    fetch(tectonicPlatesUrl)
+      .then(response => response.json())
+      .then(data => {
+        L.geoJSON(data, {
+          style: function () {
+            return {
+              color: '#000000',
+              weight: 2
+            };
+          }
+        }).addTo(tectonicPlatesLayer);
+      });
+
+    // Create legend
+    var legend = L.control({ position: "bottomright" });
+
+    legend.onAdd = function (map) {
+      var div = L.DomUtil.create("div", "legend");
+      var grades = [0, 10, 30, 50, 70, 90];
+      var legendInfo = "<h4>Depth (km)</h4>";
+
+      div.innerHTML = legendInfo;
+
+      for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+          '<i style="background:' + getMarkerColor(grades[i] + 1) + '; width:10px; height:10px; display:inline-block;"></i> ' +
+          grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+      }
+
+      return div;
+    };
+
+    // Add legend to the map
+    legend.addTo(map);
   });
 
-// Retrieve tectonic plates GeoJSON data and add to the map
-fetch('data/PB2002_plates.json')
-  .then(response => response.json())
-  .then(data => {
-    L.geoJSON(data, {
-      style: function (feature) {
-        return {
-          color: '#ff0000',
-          weight: 2
-        };
-      }
-    }).addTo(tectonicPlatesLayer);
-  });
+
 
 
 
